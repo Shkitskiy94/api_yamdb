a0adec90e044cd4d112b29dc2cb48c8763c06049
Yuriy Shkitskiy <shkitskiy94@gmail.com>
2022-11-05 22:59:56 +0300

    Исправление замечаний pep8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('v1/auth/token/', get_token, name='get_token'),
     path('v1/users/me/', UserInfo.as_view(), name='user_info'),
     path('v1/', include(router_v1.urls)),
-    ]
+]
